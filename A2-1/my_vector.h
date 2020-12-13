2847e40cbde562cb54d186ac69438c04bf05a395
Can Cölükoglu <cancoel@icloud.com>
2020-12-13 20:02:31 +0100

    added the default constructor and some definitions

--- A2-1/my_vector.h
+++ A2-1/my_vector.h
@@ -1,5 +1,27 @@
 #pragma once
-#include <array>
 
-using namespace my
+namespace my
+{
+ template<typename T>
 
+    class my_vector
+    {
+
+    public:
+
+    Vector() {} // Default constructor
+    Vector(size_t t_size) {}  
+ 
+
+    Vector& operator=(const Vector& other) {} // kopieren
+    Vector& operator=(Vector&& other) {} // bewegen 
+
+    vector(vector<ValueT> const &) = delete;
+    vector<ValueT>& operator=(vector<ValueT> const &) = delete;
+
+    private:
+
+  
+};
+
+}
